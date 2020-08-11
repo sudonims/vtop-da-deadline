a2187074f2575c56274001e59cc92d1437f398b6
Nimish Shah <nimishshah2000@gmail.com>
2020-08-11 20:00:32 +0530

    more efficiency

--- content.js
+++ content.js
@@ -3,11 +3,11 @@ function find_right_due(table_inner) {
     return new Promise(function (resolve) {
         for (let i = 0; i < table_inner.children.length; i++) {
             var check = table_inner.children[i].children[6].children.length;
-            if (check == 0){
+            if (check == 0) {
                 var dwnld = table_inner.children[i].children[5].children.length;
-                resolve({ 
-                    due:table_inner.children[i].children[4].children[0].innerHTML,
-                    download: dwnld>0 ? table_inner.children[i].children[5].children[0].children[0] : document.createElement('div')
+                resolve({
+                    due: table_inner.children[i].children[4].children[0].innerHTML,
+                    download: dwnld > 0 ? table_inner.children[i].children[5].children[0].children[0] : document.createElement('div')
                 });
             }
         }
@@ -19,49 +19,33 @@ function find_right_due(table_inner) {
 }
 
 document.addEventListener('click', async function () {
-    // alert('hi');
-    // if (!chrome.cookies) {
-    //     chrome.cookies = chrome.experimental.cookies;
-    // }
-    // await chrome.runtime.sendMessage({ cookie: true });
     // Get Reg. No
     var regNo = document.getElementsByClassName('VTopHeaderStyle')[0].children[1].innerHTML.slice(0, 9);
-    // var title = document.getElementsByClassName('box-title');
-    // title[0].innerHTML = "Lol"
-
     var table = document.getElementsByClassName('customTable')[0].children[0]
-    console.log(typeof table.children)
 
     for (let i = 1; i < table.children.length; i++) {
         var classid = table.children[i].children[1].innerHTML;
 
-        await fetch('https://vtop.vit.ac.in/vtop/examinations/processDigitalAssignment', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                'X-Requested-With': 'XMLHttpRequest'
-            },
-            body: `authorizedID=${regNo}&x=${new Date().toGMTString()}&classId=${classid}`
-        }).then(res => res.text()).then(async data => {
-            var parser = new DOMParser()
-            var doc = parser.parseFromString(data, 'text/html');
-            // console.log(data)
-            var table_inner = doc.getElementsByClassName('customTable')[1].children[1];
-            
-            // var due_date = table_inner.children[0].children[4].children[0].innerHTML;
-            var due_date = await find_right_due(table_inner).then(data => {
-                // console.log(data.download);
-                return data;
-            });
-            // console.log(due_date)
-            // newel.innerHTML = due_date
-            // table.children[i].appendChild(newel)
-            // console.log('lentgh',table.children[i].children[3].children.length)
-            if (table.children[i].children[3].children.length == 0){
+        if (table.children[i].children[3].children.length == 0) {
+            await fetch('https://vtop.vit.ac.in/vtop/examinations/processDigitalAssignment', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    'X-Requested-With': 'XMLHttpRequest'
+                },
+                body: `authorizedID=${regNo}&x=${new Date().toGMTString()}&classId=${classid}`
+            }).then(res => res.text()).then(async data => {
+                var parser = new DOMParser()
+                var doc = parser.parseFromString(data, 'text/html');
+
+                var table_inner = doc.getElementsByClassName('customTable')[1].children[1];
+
+                var due_date = await find_right_due(table_inner).then(data => data);
+
                 table.children[i].children[3].innerHTML += `<span style="display:inline; float:right;">${due_date.due}</span>`
                 table.children[i].children[3].children[0].appendChild(due_date.download)
-            }
-        }).catch(err => console.log(err));
+            }).catch(err => console.log(err));
+        }
 
         // var xhr = new XMLHttpRequest();
         // xhr.open("POST","https://vtop.vit.ac.in/vtop/examinations/processDigitalAssignment",true);
@@ -73,6 +57,5 @@ document.addEventListener('click', async function () {
         //     var response_link = xhr.response;
         //     console.log(a,response_link);
         // }
-        // console.log(a)
     }
 })
